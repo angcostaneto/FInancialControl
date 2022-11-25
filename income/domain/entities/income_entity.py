367e71e11ef81e067e1838ca53e7d65c9f678224
Angelo Costa Neto <angelo_costa_neto@outlook.com>
2022-11-24 23:13:54 -0300

    Correct decimal definition

--- income/domain/entities/income_entity.py
+++ income/domain/entities/income_entity.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class IncomeEntity(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    value = models.DecimalField(decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    value = models.DecimalField(max_digits=15, decimal_places=6)
+    created_at = models.DateTimeField(auto_now_add = True, auto_now = False, blank = True)
+    updated_at = models.DateTimeField(auto_now=True, blank = True)
     user = models.ForeignKey(User, on_delete = models.CASCADE, blank = True, null = True)
